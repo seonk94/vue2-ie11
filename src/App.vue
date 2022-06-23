7a2996104082f2f6451b2eae87c66ccdec48fee4
seonk94 <seonk94@naver.com>
2022-06-23 20:55:54 +0900

    feat

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <div class="swiper">
+      <!-- Additional required wrapper -->
+      <div class="swiper-wrapper">
+        <!-- Slides -->
+        <div class="swiper-slide">Slide 1</div>
+        <div class="swiper-slide">Slide 2</div>
+        <div class="swiper-slide">Slide 3</div>
+        ...
+      </div>
+      <!-- If we need pagination -->
+      <div class="swiper-pagination"></div>
+
+      <!-- If we need navigation buttons -->
+      <div class="swiper-button-prev"></div>
+      <div class="swiper-button-next"></div>
+
+      <!-- If we need scrollbar -->
+      <div class="swiper-scrollbar"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Swiper from "swiper";
+
+import "swiper/swiper-bundle.css";
+
+export default {
+  name: "App",
+  components: {},
+  mounted() {
+    new Swiper(".swiper", {});
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  devServer: {
+    inline: false,
+  },
+  transpileDependencies: ["sockjs-client", "swiper", "ssr-window", "dom7"],
+};
